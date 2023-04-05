09a28a774f9ab952d3a5f3ae38c4838f4be0cfc1
PaTuanas <91712990+PaTuanas@users.noreply.github.com>
2023-04-05 12:58:56 +0700

    Update#7
    Change format file

--- src/App.js
+++ src/App.js
@@ -3,31 +3,16 @@ import './App.css';
 
 import TopHeader from './components/top_header/TopHeader';
 import SideBar from './components/side_bar/SideBar';
-import Calendar from  './components/calendar/Calendar'
+import Calendar from  './components/calendar/calendar';
 import Notification from './components/notification/Notification';
 import MainContent from './components/content/MainContent';
+import Home from './screen/Home/Home';
 
 function App() {
   return (
    
     <div className='app_js'>
-      <div className='Header'>
-        <TopHeader/>
-      </div>
-      <div className='Body'> 
-        <div className='SideBar'>
-          <SideBar/>
-        </div>
-        <div className='Main-Content'>
-          <MainContent/>
-        </div>
-        <div className='Right-Cont'>
-          <Calendar/>
-          <Notification/>
-        </div>
-      </div>
-      
-    
+      <Home/>
     </div>
     
     
